29909ec2dc2538f313988a9057d0e1c48093f769
cacarulo4444 <cacarulo4444@gmail.com>
2026-02-24 14:42:22 -0300

    *****

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,7 +1,7 @@
 # catalogo/admin.py
 from django.contrib import admin
-from .models import Categoria, Producto, ConfiguracionSitio, Pedido, DetallePedido
-from .models import Talle # Importalo
+from .models import Categoria, Producto, Talle, ConfiguracionSitio, Pedido, DetallePedido
+
 
 admin.site.register(Talle)
 # Esto hace que veas los productos adentro del mismo pedido
@@ -15,5 +15,6 @@ class PedidoAdmin(admin.ModelAdmin):
 
 admin.site.register(Categoria)
 admin.site.register(Producto)
+admin.site.register(Talle)
 admin.site.register(ConfiguracionSitio)
-admin.site.register(Pedido, PedidoAdmin) # Registramos el Pedido
+admin.site.register(Pedido, PedidoAdmin) 
