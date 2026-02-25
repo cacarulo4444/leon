a93afe415eb2c8a868c1cf9cd908aa73b8689504
devko <cacarulo4444@gmail.com>
2026-02-25 15:11:14 -0300

    b

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# LA LÍNEA MÁGICA: Crea el usuario 'emi' si no existe todavía
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='dekvo').exists() or User.objects.create_superuser('dekvo', 'dekvo@tienda.com', 'Caca4444')"
